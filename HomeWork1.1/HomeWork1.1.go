bb8315157085cea769f4073b23909ec306762ff4
NETSRV\jasper <ddolgosheev2@gmail.ru>
2022-05-05 22:51:17 +0600

    Домашняя работа v0.2

--- HomeWork1.1/HomeWork1.1.go
+++ HomeWork1.1/HomeWork1.1.go
@@ -5,24 +5,59 @@
  *                  -   в) *используя вывод со знаком $.
  */
 
+//Анкета
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+//Основная функция
 func main() {
-	var firstName, lastName string
-	var age, growth, weight int
 
-	fmt.Println("Введите имя ?")
-	fmt.Scan(&firstName)
-	fmt.Println("Введите фамилию ?")
-	fmt.Scan(&lastName)
-	fmt.Println("Введите возраст ?")
-	fmt.Scan(&age)
-	fmt.Println("Введите рост?")
-	fmt.Scan(&growth)
-	fmt.Println("Введите вес ?")
-	fmt.Scan(&weight)
+	//Переменная для использования интерфейса io.Reader
+	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Printf("\n Вас зовут : %s %s\n Ваш возраст : %d\n Ваш рост : %d\n Ваш вес : %d\n", firstName, lastName, age, growth, weight)
+	//Ввод имени
+	fmt.Println("Введите ваше имя :")
+	inputFirstName, _ := reader.ReadString('\n')
+	if len(inputFirstName) <= 2 {
+		fmt.Println("Вводи свое имя!")
+		return
+	}
+	//Ввод фамилии
+	fmt.Println("Введите фамилию :")
+	inputLastName, _ := reader.ReadString('\n')
+	if len(inputLastName) <= 2 {
+		fmt.Println("Вводи свою фамилию!")
+		return
+	}
+	//Ввод возраста
+	fmt.Println("Введите возраст :")
+	inputAge, _ := reader.ReadString('\n')
+	if len(inputAge) <= 2 {
+		fmt.Println("Вводи свой возраст!")
+		return
+	}
+	//Ввод роста
+	fmt.Println("Введите рост :")
+	inputGrowth, _ := reader.ReadString('\n')
+	if len(inputGrowth) <= 2 {
+		fmt.Println("Вводи свой рост!")
+		return
+	}
+	//Ввод веса
+	fmt.Println("Введите вес :")
+	inputWeight, _ := reader.ReadString('\n')
+	if len(inputWeight) <= 2 {
+		fmt.Println("Вводи свой вес!")
+		return
+	}
+
+	//Вывод введенных параметров
+	fmt.Printf("\n Вас зовут : \n %v %v Ваш возраст : \n %v\n Ваш рост : \n %v\n Ваш вес : \n %v\n",
+		strings.ToUpper(inputFirstName), strings.ToUpper(inputLastName), inputAge, inputGrowth, inputWeight)
 }
